2b7bce6c5f61196fa3e1f5eca4396baf7b3f70a6
Fer <ferp@protonmail.ch>
2024-08-11 20:16:38 +0200

    play with color intensity and elevation to fake a shadow

--- src/shaders/test/fragment.glsl
+++ src/shaders/test/fragment.glsl
@@ -4,10 +4,12 @@
     uniform sampler2D uTexture;
 
     varying vec2 vUv;
+    varying float vElevation;
 
     void main()
     {
 
       vec4 textureColor = texture2D(uTexture, vUv);
+      textureColor.rgb *= vElevation * 2.0 + 1.0;
       gl_FragColor = vec4(textureColor);
     }
